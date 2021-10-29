ef3c612fd47c3c8e702a494d4129a5531b41d3f7
zhaoml <menglong.zhao@geekbang.com>
2021-10-29 18:30:43 +0800

    玩转Vue 3全家桶 第六章

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>这是首页</h1>
+  <h2>{{ x }}, {{ y }}</h2>
+  <TodoList />
+</template>
+
+<script setup>
+import TodoList from '../components/Todolist.vue'
+import {useMouse} from "../utils/mouse";
+let {x,y} = useMouse()
+</script>
+
+<style scoped>
+
+</style>
