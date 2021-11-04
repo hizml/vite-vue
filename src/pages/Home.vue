01d9689c0d5162e33edbbc967cca782a0be4c8d1
zhaoml <menglong.zhao@geekbang.com>
2021-11-04 12:27:44 +0800

    玩转Vue 3全家桶 第八章

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,11 +2,22 @@
   <h1>这是首页</h1>
   <h2>{{ x }}, {{ y }}</h2>
   <TodoList />
+  <h1>你的评分是 {{score}}</h1>
+  <Rate v-model="score" theme="red" />
 </template>
 
 <script setup>
 import TodoList from '../components/Todolist.vue'
+import Rate from '../components/Rate.vue'
 import {useMouse} from "../utils/mouse";
+
+import {ref} from "vue";
+let score = ref(3.5)
+
+function update(num) {
+  score.value = num
+}
+
 let {x,y} = useMouse()
 </script>
 
