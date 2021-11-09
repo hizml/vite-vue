ae18bc004fdccd7be4623044285ea9b8606a8cb4
zhaoml <menglong.zhao@geekbang.com>
2021-11-09 10:15:45 +0800

    玩转Vue 3全家桶 第九章 这课代码太不严谨了,照着打还tm报错

---
+++ src/components/Count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="add">
+    {{ count }}
+  </div>
+</template>
+
+<script setup>
+import { useStore } from "vuex"
+import { computed } from "vue"
+
+let store = useStore()
+let count = computed(() => store.state.count)
+
+let double = computed(() => store.getters.double)
+
+function add() {
+  store.commit('add')
+}
+
+
+function asyncAdd(){
+  store.dispatch('asyncAdd')
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,7 @@
   <TodoList />
   <h1>你的评分是 {{score}}</h1>
   <Rate v-model="score" theme="red" />
+  <Count></Count>
 </template>
 
 <script setup>
@@ -12,6 +13,8 @@ import Rate from '../components/Rate.vue'
 import {useMouse} from "../utils/mouse";
 
 import {ref} from "vue";
+import Count from '../components/Count.vue'
+
 let score = ref(3.5)
 
 function update(num) {
